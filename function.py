2b56e80f77fc803ae67fad3c0734af05831240e8
Rajay <106291612+RajayGmr@users.noreply.github.com>
2023-05-22 21:41:55 +0700

    Add files via upload

--- function.py
+++ function.py
@@ -1,4 +1,7 @@
-def hello():
-    print("Hello I am Jame")
+def hello(name,lastname,age):
+    print("Hello I am "+name+ " " + lastname+ " You are "+ str(age) +" years old")
+    print("Nice to meet you")
 
-hello()
+name="James"
+myage=34
+hello(name, "Smith",myage)
